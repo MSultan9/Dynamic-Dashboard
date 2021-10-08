f8e9898fff65a4917b55fe494cf51cf7d1bdeca4
Mohamad Sultan <msultan@bluedrop.fr>
2021-10-08 09:57:05 +0300

    dashboard init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import GroupedBar  from "./charts/GroupedBar";
+import DoughnutChart from "./charts/Doughnut";
+import PieChart from "./charts/Pie";
+import LineChart from "./charts/Line";
+import Polar from "./charts/Polar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-wrapper">
+      <GroupedBar />
+      <DoughnutChart />
+      <PieChart />
+      <LineChart />
+      <Polar />
     </div>
   );
 }
